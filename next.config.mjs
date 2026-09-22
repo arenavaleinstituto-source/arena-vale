7404090fded2c66faf4193a4aca591d6567364f7
arenavaleinstituto-source <arenavaleinstituto@gmail.com>
2026-09-22 03:16:45 -0300

    Delete next.config.mjs

--- next.config.mjs
+++
@@ -1,6 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: { typedRoutes: true },
-  images: { remotePatterns: [{ protocol: 'https', hostname: '**.supabase.co' }] },
-};
-export default nextConfig;
